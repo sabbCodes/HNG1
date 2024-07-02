11b7e857434c82e3ba7e3ad5e038af29def7ff4c
Abbas Olanrewaju Sarafa <109840351+sabbCodes@users.noreply.github.com>
2024-07-02 09:15:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Function to update current time in UTC
-    function updateUTCTime() {
+    function updateLocalTime() {
         const now = new Date();
-        const options = { weekday: 'long', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZone: 'UTC' };
+        const options = {
+            weekday: 'long',
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric'
+        };
         const formattedTime = now.toLocaleDateString('en-US', options);
         document.querySelector('[data-testid="currentTimeUTC"]').textContent = formattedTime;
         document.querySelector('[data-testid="currentDay"]').textContent = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(now);
     }
 
-    // Update time initially
-    updateUTCTime();
+    updateLocalTime();
 
-    // Update time every second
-    setInterval(updateUTCTime, 1000);
+    setInterval(updateLocalTime, 1000);
 });
